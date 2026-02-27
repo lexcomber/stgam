e0a0f10d8a9a584aed1188a8d9429f0a1307d249
Lex Comber <a.comber@leeds.ac.uk>
2026-02-27 12:56:07 +0000

    update to gam_model_rank

--- R/gam_model_rank.R
+++ R/gam_model_rank.R
@@ -2,6 +2,7 @@
 #'
 #' @param res_tab a `data.frame` returned from the `evaluate_models()` function.
 #' @param n the number of ranked models to return.
+#' @param show_k whether to include the smooth knots in the output (defualt is `FALSE`)
 #'
 #' @returns a `tibble` of the 'n' best models, ranked by AIC, with the form of each predictor variable where '---' indicates the absence of a predictor, 'Fixed' that a parametric form was specified,  's(S)' a spatial smooth, 's(T)'  a temporal smooth and 'te(ST)' a combined space-time smooth. Model AIC is reported as are the knots in each smooth (`ks`) and the formula of each model (`f`).
 #' @importFrom dplyr relocate
@@ -48,9 +49,9 @@
 #'     VC_type = "SVC"
 #'   )
 #' # rank the models
-#' gam_model_rank(svc_mods)
+#' gam_model_rank(svc_mods, show_k = T)
 #' @export
-gam_model_rank <- function(res_tab, n = 10) {
+gam_model_rank <- function(res_tab, n = 10, show_k = FALSE) {
   ks_col <- which(names(res_tab) == "ks")
   smooth_cols <- 1:(ks_col-1)
   nm <- names(res_tab)
@@ -88,32 +89,34 @@ gam_model_rank <- function(res_tab, n = 10) {
     str_detect(x, "\\b(s|te|ti)\\s*\\(")
   }
 
-  for(i in 1:nrow(out_tab)) {
-    row.i <- out_tab[i,]
-    # create vector of k's allowing for non-smooth elements
-    smooth_lists <- lapply(row.i[smooth_cols], split_smooths) |> unlist()
-    smooth_counts <- sapply(smooth_lists, function(x) contains_smooth(x) + 0 |> unlist())
-    ks_s <- str_split(row.i[ks_col], ",\\s*")[[1]] %>% as.numeric()
-    ks_vec <- rep("", length(smooth_counts))
-    ks_vec[which(smooth_counts > 0)] <- ks_s
+  if (show_k) {
+    for(i in 1:nrow(out_tab)) {
+      row.i <- out_tab[i,]
+      # create vector of k's allowing for non-smooth elements
+      smooth_lists <- lapply(row.i[smooth_cols], split_smooths) |> unlist()
+      smooth_counts <- sapply(smooth_lists, function(x) contains_smooth(x) + 0 |> unlist())
+      ks_s <- str_split(row.i[ks_col], ",\\s*")[[1]] %>% as.numeric()
+      ks_vec <- rep("", length(smooth_counts))
+      ks_vec[which(smooth_counts > 0)] <- ks_s
 
-    # how many smooths in each selected column?
-    smooth_lists <- lapply(row.i[smooth_cols], split_smooths)
-    smooth_counts <- lengths(smooth_lists)
-    # cumulative positions to slice ks_vec
-    k_starts <- cumsum(c(1, head(smooth_counts, -1)))
-    k_ends   <- cumsum(smooth_counts)
-    # assign new smooths back into their columns
-    for (j in seq_along(smooth_cols)) {
-      col_index <- smooth_cols[j]
-      cell_value = smooth_lists[[j]]
-      ks_slice  <- ks_vec[k_starts[j]:k_ends[j]]
-      new_smooths <- attach_k(smooth_lists[[j]], ks_slice)
-      new_smooths <- paste(new_smooths, collapse = " + ")
-      new_smooths <- gsub(" ", "", new_smooths)
-      row.i[,j] <- new_smooths
+      # how many smooths in each selected column?
+      smooth_lists <- lapply(row.i[smooth_cols], split_smooths)
+      smooth_counts <- lengths(smooth_lists)
+      # cumulative positions to slice ks_vec
+      k_starts <- cumsum(c(1, head(smooth_counts, -1)))
+      k_ends   <- cumsum(smooth_counts)
+      # assign new smooths back into their columns
+      for (j in seq_along(smooth_cols)) {
+        col_index <- smooth_cols[j]
+        cell_value = smooth_lists[[j]]
+        ks_slice  <- ks_vec[k_starts[j]:k_ends[j]]
+        new_smooths <- attach_k(smooth_lists[[j]], ks_slice)
+        new_smooths <- paste(new_smooths, collapse = " + ")
+        new_smooths <- gsub(" ", "", new_smooths)
+        row.i[,j] <- new_smooths
+      }
+      out_tab[i,] <- row.i
     }
-    out_tab[i,] <- row.i
   }
   return(out_tab |> mutate(Rank = 1:n()) |> relocate(Rank))
 }
